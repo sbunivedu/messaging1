b679aebd1bc8cc8b107f556bd92bbb447bc94341
Baochuan Lu <lubaochuan@gmail.com>
2023-04-03 05:42:22 -0500

    use alert to delete text message

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 import React from 'react';
 
 import Status from './components/Status';
@@ -22,7 +22,35 @@ export default class App extends React.Component {
     ],
   };
 
-  handlePressMessage = () => {}
+  handlePressMessage = ({ id, type }) => {
+    switch (type) {
+      case 'text':
+        Alert.alert(
+          'Delete message?',
+          'Are you sure you want to permanently delete this message?',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel',
+            },
+            {
+              text: 'Delete',
+              style: 'destructive',
+              onPress: () => {
+                const { messages } = this.state;
+                this.setState({
+                  messages: messages.filter(
+                    message => message.id !== id,
+                  ),
+                });
+              },
+            },
+          ], );
+        break;
+      default:
+        break;
+    }
+  };
 
   renderMessageList() {
     const { messages } = this.state;
