fa329b87dd9600d9f839b91f23ba30664026e1b5
Baochuan Lu <lubaochuan@gmail.com>
2023-04-03 05:56:24 -0500

    use alert to show fullscreen image

--- App.js
+++ App.js
@@ -1,4 +1,12 @@
-import { StyleSheet, View, Alert } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Alert,
+  Image,
+  TouchableHighlight,
+  BackHandler,
+ } from 'react-native';
+
 import React from 'react';
 
 import Status from './components/Status';
@@ -20,6 +28,34 @@ export default class App extends React.Component {
         longitude: -122.4324,
       }),
     ],
+    fullscreenImageId: null,
+  };
+
+  renderFullscreenImage = () => {
+    const { messages, fullscreenImageId } = this.state;
+
+    if (!fullscreenImageId) return null;
+
+    const image = messages.find(
+      message => message.id === fullscreenImageId,
+    );
+
+    if (!image) return null;
+
+    const { uri } = image;
+
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={this.dismissFullscreenImage} >
+        <Image style={styles.fullscreenImage} source={{ uri }} />
+      </TouchableHighlight>
+    );
+  };
+
+
+  dismissFullscreenImage = () => {
+    this.setState({ fullscreenImageId: null });
   };
 
   handlePressMessage = ({ id, type }) => {
@@ -46,6 +82,9 @@ export default class App extends React.Component {
               },
             },
           ], );
+        break;
+      case 'image':
+        this.setState({ fullscreenImageId: id });
         break;
       default:
         break;
@@ -83,8 +122,26 @@ export default class App extends React.Component {
         {this.renderMessageList()}
         {this.renderToolbar()}
         {this.renderInputMethodEditor()}
+        {this.renderFullscreenImage()}
       </View>
     );
+  }
+
+  componentDidMount() {
+    this.subscription = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        const { fullscreenImageId } = this.state;
+        if (fullscreenImageId) {
+          this.dismissFullscreenImage();
+          return true;
+        }
+        return false;
+      },
+    );
+  }
+  componentWillUnmount() {
+    this.subscription.remove();
   }
 }
 
@@ -105,5 +162,14 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: 'rgba(0,0,0,0.04)',
     backgroundColor: 'white',
+  },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 2,
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: 'contain',
   },
 });
